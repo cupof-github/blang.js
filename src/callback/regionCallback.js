037842c55eb0400f0f6bd34fdfef56489beddd58
cupof <mail.cupof@gmail.com>
2018-06-30 03:34:02 +0900

    fix bug

--- src/callback/regionCallback.js
+++ src/callback/regionCallback.js
@@ -10,25 +10,28 @@ export default (arg, lang, target, regions) => {
   // comparing language whether matching method and default
   if (lang.slice(0, 2) !== target) return false;
 
-  // local region '*-us', '*-ca' and so.
-  let local = lang.slice(3, 5);
-  let argument = toLower(arg);
+  if (arg !== undefined) {
+    let local = lang.slice(3, 5);
 
-  // if arg is array
-  if (Array.isArray(arg)) {
-    // if browser-lang has nsetting as 'en-us', 'zh-tw', 'de-de'
-    if (local !== "") {
-      // check invalid regions in array
-      regionCheck(argument, regions, target);
+    // if arg is array
+    if (Array.isArray(arg)) {
+      // local region '*-us', '*-ca' and so.
+      let argument = toLower(arg);
+      // if browser-lang has nsetting as 'en-us', 'zh-tw', 'de-de'
+      if (local !== "") {
+        // check invalid regions in array
+        regionCheck(argument, regions, target);
 
-      return contains(argument, local);
-    }
-    // if lang is plain like, 'en', 'zh', 'de'
-    return true;
-  } else {
-    // if arg is string
-    return arg.toLowerCase() === local ? true : false;
-  } // ! Array.isArray
+        return contains(argument, local);
+      }
+      // if lang is plain like, 'en', 'zh', 'de'
+      return true;
+    } else {
+      // if arg is string
+      return arg.toLowerCase() === local ? true : false;
+    } // ! Array.isArray
+  }
+
   // if arg is empty
   return true;
 };
